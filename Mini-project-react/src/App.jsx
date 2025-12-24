c68ea4e9bdcbf313fa6dfda5ce3e0c4b376a0e29
p-k2 <kaurpalak193@gmail.com>
2025-12-24 15:02:04 +0530

    Initial half-done project files

---
+++ Mini-project-react/src/App.jsx
@@ -0,0 +1,19 @@
+
+// import './App.css'
+// import Button from "@mui/material/Button" ;
+import SearchBox from "./SearchBox"
+
+function App() {
+ let handleClick = ()=>{
+      console.log("Button was Clicked!")
+    }
+  return (
+   
+    <>
+    <SearchBox/>
+   
+    </>
+  )
+}
+
+export default App
---
+++ Mini-project-react/src/SearchBox.jsx
@@ -0,0 +1,37 @@
+import TextField from '@mui/material/TextField';
+import Button from "@mui/material/Button" ;
+import "./SearchBox.css";
+import { useState } from 'react';
+
+export default function SearchBox(){
+ let [city, setCity ] = useState("");
+
+    let handleChange = (evt) => {
+        setCity(evt.target.value) ;
+    }
+    let handleSubmit = (evt)=>{
+
+        evt.preventDefault();
+        console.log(city) ;
+        setCity("") ;
+    }
+    return(
+        <div className = "SearchBox">
+          <h3> Search for weather</h3>  
+          <form onSubmit = {handleSubmit}>
+            <TextField id="city" 
+            label="City Name"
+             variant="outlined"
+              required
+
+              onChange= {handleChange}/>
+            <br></br>
+            <br></br>
+            <Button variant = "contained" type="Submit">
+                Search 
+            </Button>
+            </form> 
+        </div>
+       
+    )
+}
